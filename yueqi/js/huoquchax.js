3ac03211a21da2131323966b5ace7b5386fa54e1
993619582 <993619582@qq.com>
2018-07-04 15:55:50 +0800

    11

--- yueqi/js/huoquchax.js
+++ yueqi/js/huoquchax.js
@@ -1,66 +1,114 @@
 const express = require('express');
 const path = require('path');
 const async = require('async');
+const waterfall = require('async/waterfall');
+
 
 const mysql = require(path.dirname(__dirname) + "/lib/mysql.js")('yueqi');
-module.exports = function (db, shousuoci,fengye, res) {
+module.exports = function (db, shousuoci, fengye, res) {
   var shoutui;
   var coll = [],
     nocoll = [];
   if (shousuoci == '') {
     //定义SQL语句
-
-
-    var $totalsql = 'SELECT count(product_pname) AS totalnum FROM product_particular WHERE yesno=0';
-    
-    var $sql1st = 'SELECT pdpctid,pdtid,product_pname,suggestmoney,money,adminid,particular,shopid,\
+    async.waterfall([function (callback) {
+        var $totalsql = 'SELECT count(product_pname) AS totalnum FROM product_particular WHERE yesno=0';
+        mysql.query($totalsql, function (err, result) {
+          console.log(result)
+          if (result) {
+            callback(null, result[0].totalnum);
+          }
+        });
+      },
+      function (arg1, callback) {
+        //定义每页显示多少条数据
+        var pagenum = 10;
+        //当前的页数
+        var page = fengye;
+        if (page < 1) page = 1;
+        //起始位
+        var start = pagenum * (page - 1);
+        //共有多少页：(总条数/pagenum)  向上取整
+        totalpage = Math.ceil(arg1 / pagenum);
+        var $sql1st = 'SELECT pdpctid,pdtid,product_pname,suggestmoney,money,adminid,particular,shopid,\
       shotel FROM `product_particular` WHERE yesno=0  LIMIT ?, ?';
-    console.log(shousuoci)
-    var $sql1stimg = 'SELECT imgurl FROM `product_particular_img` WHERE pdpctid = ? AND yesno=0';
-    mysql.query($sql1st, [start, pagenum], function (err, result) {
-      console.log(err, 2)
-      if (result) {
-        console.log(result, 1)
-        for (let i = 0; i < result.length; i++) {
-          coll.push(i);
-        }
-        async.each(coll, function (item, callback) {
-            // console.log(item)
-            mysql.query($sql1stimg, result[item].pdpctid, function (err11, namedata) {
-              // console.log(namedata)
-              var data1 = [];
-              if (err11) {
-                nocoll.push(item);
-                callback();
-              } else {
-                for (var i1 = 0; i1 < namedata.length; i1++) {
-                  shdz = namedata[i1].imgurl;
-                  data1.push(shdz);
-                  // if (i1 == 0) {
-                  //   data1[0] = shdz;
-                  // } else {
+        console.log(arg1, totalpage)
+        mysql.query($sql1st, [start, pagenum], function (err, result) {
+          console.log(err, 2)
+          if (result) {
+            console.log(result, 1)
+            for (let i = 0; i < result.length; i++) {
+              coll.push(i);
+            }
+            console.log(coll);
+            callback(null, coll, result);
 
-                  // }
-                }
-                result[item]["imgurl"] = data1;
-                callback();
-              }
-            });
-          },
-          function (err) {
-            shoutui = result;
-            console.log(result)
-            res.render('view/zslb', {
-              dengl: db,
-              shouye: shoutui
-            });
-            return shoutui;
+          }
+        });
+      },
+      function (arg1, arg2,callback) {
+        console.log(arg1,arg2)
 
-          });
-      } else {
-        return shoutui;
+        callback(null, 'done');
       }
-    });
+    ], function (err, result) {
+      console.log(result)
+      res.render('view/zslb', {
+        dengl: db,
+        shouye: ''
+      });
+    })
+
+    // var $totalsql = 'SELECT count(product_pname) AS totalnum FROM product_particular WHERE yesno=0';
+
+    // var $sql1st = 'SELECT pdpctid,pdtid,product_pname,suggestmoney,money,adminid,particular,shopid,\
+    //   shotel FROM `product_particular` WHERE yesno=0  LIMIT ?, ?';
+    // console.log(shousuoci)
+    // var $sql1stimg = 'SELECT imgurl FROM `product_particular_img` WHERE pdpctid = ? AND yesno=0';
+    // mysql.query($sql1st, [start, pagenum], function (err, result) {
+    //   console.log(err, 2)
+    //   if (result) {
+    //     console.log(result, 1)
+    //     for (let i = 0; i < result.length; i++) {
+    //       coll.push(i);
+    //     }
+    //     async.each(coll, function (item, callback) {
+    //         // console.log(item)
+    //         mysql.query($sql1stimg, result[item].pdpctid, function (err11, namedata) {
+    //           // console.log(namedata)
+    //           var data1 = [];
+    //           if (err11) {
+    //             nocoll.push(item);
+    //             callback();
+    //           } else {
+    //             for (var i1 = 0; i1 < namedata.length; i1++) {
+    //               shdz = namedata[i1].imgurl;
+    //               data1.push(shdz);
+    //               // if (i1 == 0) {
+    //               //   data1[0] = shdz;
+    //               // } else {
+
+    //               // }
+    //             }
+    //             result[item]["imgurl"] = data1;
+    //             callback();
+    //           }
+    //         });
+    //       },
+    //       function (err) {
+    //         shoutui = result;
+    //         console.log(result)
+    //         res.render('view/zslb', {
+    //           dengl: db,
+    //           shouye: shoutui
+    //         });
+    //         return shoutui;
+
+    //       });
+    //   } else {
+    //     return shoutui;
+    //   }
+    // });
   } else {
     var $sql1st = 'SELECT pdpctid,pdtid,product_pname,suggestmoney,money,adminid,particular,shopid,\
       shotel FROM `product_particular` WHERE product_pname LIKE "%"?"%" || shopid LIKE "%"?"%"';
